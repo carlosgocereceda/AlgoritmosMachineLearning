4a866a0f8ee05b4e804fd05e049f021c9026b904
carlosgocereceda <carlosgcereceda@hotmail.es>
2019-05-08 11:04:38 +0200

    bayes casi acabado a falta de pertencencia

--- src/negocio/Bayes.java
+++ src/negocio/Bayes.java
@@ -8,13 +8,12 @@ public class Bayes {
 
 	private Dato m1, m2;
 	private ArrayList<ArrayList<Double>> c1, c2;
-	private Datos datos;
 
 	public void algoritmo(Datos datos) {
 		m1 = calculaM(datos, "Iris-setosa");
 		m2 = calculaM(datos, "Iris-versicolor");
-		c1 = calculaCentro(datos, "Iris-setosa");
-		c2 = calculaCentro(datos, "Iris-versicolor");
+		c1 = calculaCentro(new ArrayList<Dato>( datos.getData().subList(0, 50)), "Iris-setosa");
+		c2 = calculaCentro(new ArrayList<Dato>( datos.getData().subList(50, 100)), "Iris-versicolor");
 		claseALaQuePertenece(new Dato(5.1, 3.5, 1.4, 0.2, "Iris-setosa"));
 	}
 
@@ -37,43 +36,29 @@ public class Bayes {
 		return dato;
 	}
 
-	private ArrayList<ArrayList<Double>> calculaCentro(Datos datos, String clase) {
-		Dato x_m1 = new Dato(datos.getData().get(0).getD1() - this.m1.getD1(), datos.getData().get(0).getD2() - this.m1.getD2(), datos.getData().get(0).getD3() - this.m1.getD3(),
-				datos.getData().get(0).getD4() - this.m1.getD4(), "");
-		// x-m2
-		Dato x_m2 = new Dato(datos.getData().get(0).getD1() - this.m2.getD1(), datos.getData().get(0).getD2() - this.m2.getD2(), datos.getData().get(0).getD3() - this.m2.getD3(),
-				datos.getData().get(0).getD4() - this.m2.getD4(), "");
+	private ArrayList<ArrayList<Double>> calculaCentro(ArrayList<Dato> d, String clase) {
+		Dato m = null;
+		if(clase.equalsIgnoreCase("Iris-setosa")) m = m1;
+		else m = m2;
+		Dato x_m = new Dato(d.get(0).getD1() - m.getD1(), d.get(0).getD2() - m.getD2(), d.get(0).getD3() - m.getD3(),
+				d.get(0).getD4() - m.getD4(), "");
 
-		double[][] x_m1_aux = { { x_m1.getD1(), x_m1.getD2(), x_m1.getD3(), x_m1.getD4() } };
-		double[][] x_m2_aux = { { x_m2.getD1(), x_m2.getD2(), x_m2.getD3(), x_m2.getD4() } };
-		Matrix x_m1_matrix = new Matrix(x_m1_aux);
-		Matrix x_m2_matrix = new Matrix(x_m2_aux);
-		Matrix sol_aux1 = x_m1_matrix.transpose().times(x_m1_matrix);
-		Matrix sol_aux2 = x_m2_matrix.transpose().times(x_m2_matrix);
-		double n1 = 1.00;
-		double n2 = 0.00;
-		for (int i = 1; i < datos.getData().size(); i++) {
-			if (datos.getData().get(i).getClase().equalsIgnoreCase(clase)) {
+		double[][] x_m_aux = { { x_m.getD1(), x_m.getD2(), x_m.getD3(), x_m.getD4() } };
+		Matrix x_m_matrix = new Matrix(x_m_aux);
+		Matrix sol_aux = x_m_matrix.transpose().times(x_m_matrix);
+		double n = 1.00;
+		for (int i = 1; i < d.size(); i++) {
+			if (d.get(i).getClase().equalsIgnoreCase(clase)) {
 				// x-m1
-				x_m1 = new Dato(datos.getData().get(i).getD1() - this.m1.getD1(), datos.getData().get(i).getD2() - this.m1.getD2(), datos.getData().get(i).getD3() - this.m1.getD3(),
-						datos.getData().get(i).getD4() - this.m1.getD4(), "");
-				double[][] x_m1_aux_ = { { x_m1.getD1(), x_m1.getD2(), x_m1.getD3(), x_m1.getD4() } };
-				Matrix x_m1_matrix_ = new Matrix(x_m1_aux_);
-				sol_aux1 = sol_aux1.plus(x_m1_matrix_.transpose().times(x_m1_matrix_));
-				n1++;
-			}
-			else {
-				// x-m2
-				x_m2 = new Dato(datos.getData().get(i).getD1() - this.m2.getD1(), datos.getData().get(i).getD2() - this.m2.getD2(), datos.getData().get(i).getD3() - this.m2.getD3(),
-						datos.getData().get(i).getD4() - this.m2.getD4(), "");
-				double[][] x_m2_aux_ = { { x_m2.getD1(), x_m2.getD2(), x_m2.getD3(), x_m2.getD4() } };
-				Matrix x_m2_matrix_ = new Matrix(x_m2_aux_);
-				sol_aux2 = sol_aux2.plus(x_m2_matrix_.transpose().times(x_m2_matrix_));
-				n2++;
+				x_m = new Dato(d.get(i).getD1() - m.getD1(), d.get(i).getD2() - m.getD2(), d.get(i).getD3() - m.getD3(),
+						d.get(i).getD4() - m.getD4(), "");
+				double[][] x_m_aux_ = { { x_m.getD1(), x_m.getD2(), x_m.getD3(), x_m.getD4() } };
+				Matrix x_m_matrix_ = new Matrix(x_m_aux_);
+				sol_aux = sol_aux.plus(x_m_matrix_.transpose().times(x_m_matrix_));
+				n++;
 			}
 		}
-		sol_aux1 = sol_aux1.times(1/n1);
-		sol_aux2 = sol_aux2.times(1/n2);
+		sol_aux = sol_aux.times(1/n);
 		System.out.println("P");
 		return null;
 	}
